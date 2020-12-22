2cc49c39ec86036d2fef3a240b183fbc10a9a5fe
fonzerelly <christian.hoerauf@gmail.com>
2020-12-22 18:10:14 +0100

    refactor tests

--- test.py
+++ test.py
@@ -4,8 +4,11 @@ from tower import createTowerAt
 
 from Position import Position
 
-def exact (list):
-    filter(lambda t: t[0] == )
+
+
+exact = lambda fst: lambda snd: fst[0] == snd[0] and fst[1] == snd[1] and fst[2] == snd[2]
+curriedFilter = lambda predicate: lambda l: list(filter(predicate, l))
+findExactlyIn = lambda tpl: lambda lst: len(curriedFilter(exact(tpl))(lst)) == 1
 
 class TestCreateTowerAt(unittest.TestCase):
     def setUp(self):
@@ -13,35 +16,35 @@ class TestCreateTowerAt(unittest.TestCase):
         self.resultBottom = createTowerAt(p)
 
     def test_should_position_center_of_tower(self):
-        self.resultBottom.index((-1, 0, 4))
+        self.assertTrue(findExactlyIn((-1, 0, 4))(self.resultBottom))
 
     def test_should_position_center_according_to_passed_in_position(self):
         p = Position(1, 2, 3)
         result = createTowerAt(p)
-        result.index((0, 2, 7))
+        self.assertTrue(findExactlyIn((0, 2, 7))(result))
 
     def test_should_have_wall_to_the_right(self):
-        self.resultBottom.index((1, 0, 2))
-        self.resultBottom.index((1, 0, 3))
-        self.resultBottom.index((1, 0, 4))
-        self.resultBottom.index((1, 0, 5))
-        self.resultBottom.index((1, 0, 6))
+        self.assertTrue(findExactlyIn((1, 0, 2))(self.resultBottom))
+        self.assertTrue(findExactlyIn((1, 0, 3))(self.resultBottom))
+        self.assertTrue(findExactlyIn((1, 0, 4))(self.resultBottom))
+        self.assertTrue(findExactlyIn((1, 0, 5))(self.resultBottom))
+        self.assertTrue(findExactlyIn((1, 0, 6))(self.resultBottom))
 
     def test_should_have_wall_to_the_left(self):
-        self.resultBottom.index((-3, 0, 2))
-        self.resultBottom.index((-3, 0, 3))
-        self.resultBottom.index((-3, 0, 4))
-        self.resultBottom.index((-3, 0, 5))
-        self.resultBottom.index((-3, 0, 6))
+        self.assertTrue(findExactlyIn((-3, 0, 2))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-3, 0, 3))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-3, 0, 4))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-3, 0, 5))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-3, 0, 6))(self.resultBottom))
 
     def test_should_have_wall_at_back(self):
-        self.resultBottom.index((-2, 0, 6))
-        self.resultBottom.index((-1, 0, 6))
-        self.resultBottom.index(( 0, 0, 6))
+        self.assertTrue(findExactlyIn((-2, 0, 6))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-1, 0, 6))(self.resultBottom))
+        self.assertTrue(findExactlyIn(( 0, 0, 6))(self.resultBottom))
 
     def test_should_have_wall_at_front_with_door(self):
-        self.resultBottom.index((-2, 0, 2))
-        self.resultBottom.index((-1, 0, 2))
+        self.assertTrue(findExactlyIn((-2, 0, 2))(self.resultBottom))
+        self.assertTrue(findExactlyIn((-1, 0, 2))(self.resultBottom))
 
     
     
