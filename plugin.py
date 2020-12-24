2e8eb6a870db7b06c2456e2a4cf474d61116dbb1
fonzerelly <christian.hoerauf@gmail.com>
2020-12-24 07:50:13 +0100

    forwards height as argument. dont waste time in doing output in code outside of plugin

--- plugin.py
+++ plugin.py
@@ -8,9 +8,15 @@ class KuerbisPlugin(PythonPlugin):
     def onCommand(self, sender, command, label, args):
         welt = sender.getWorld()
         position = sender.getLocation()
+        self.getLogger().info("************** creating a tower of height: " + args[0])
+        height = 0
+        try:
+            height = int(args[0])
+        except Exception as e:
+            self.getLogger().info("!!!!!!!!!!!! Failed to convert param:" + args[0])
+            self.getLogger().info("!!!!!!!!!!!! Ignored to build Tower")
         
-        for tuple in createTowerAt(position,10):
-            self.getLogger().info("********************" + str(tuple))
+        for tuple in createTowerAt(position, height):
             l = Location (welt, tuple[0], tuple[1], tuple[2])
             block = welt.getBlockAt(l)
             block.setType(bukkit.Material.COBBLESTONE)
